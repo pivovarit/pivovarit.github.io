3ee44291892622ebbd4a257ace4539fc1aa19e47
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2017-11-05 13:42:47 +0200

    Redirect to 4Comprehension.com

--- js/main.js
+++ js/main.js
@@ -18,21 +18,22 @@ $(document).ready(function () {
   })
 
   if (window.location.hash && window.location.hash == '#blog') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
+   /* $('.panel-cover').addClass('panel-cover--collapsed')*/
   }
 
   if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
+    /*$('.panel-cover').addClass('panel-cover--collapsed')*/
   }
 
   $('.btn-mobile-menu').click(function () {
-    $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+   /* $('.navigation-wrapper').toggleClass('visible animated bounceInDown')
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')*/
   })
 
   $('.navigation-wrapper .blog-button').click(function () {
-    $('.navigation-wrapper').toggleClass('visible')
-    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+   /* $('.navigation-wrapper').toggleClass('visible')
+    $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')*/
+      window.location.href = "http://4comprehension.com";
   })
 
 })
