545480860b3d752c31ac6370f3e801fed38c2a73
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2018-04-07 17:33:29 +0200

    Update index.js

--- talks/index.js
+++ talks/index.js
@@ -13,7 +13,7 @@ function initMap() {
 
 	var map = new google.maps.Map(document.getElementById('map'), {
 	  zoom: 2,
-	  center: {lat: 50, lng: -20}
+	  center: {lat: 20, lng: -20}
 	});
 	let places = findPlaces();
 	for(place in places) {
