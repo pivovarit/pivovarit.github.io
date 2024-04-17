030c33e3937f17fcca84992690ff369f08713600
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-04-17 08:58:48 +0200

    use AdvancedMarkerElement

--- talks/index.js
+++ talks/index.js
@@ -1,7 +1,7 @@
 function initMap() {
-	function findPlaces(year) {
-		let locations = document.querySelectorAll('[data-location]');
-		let all = {};
+    function findPlaces(year) {
+        let locations = document.querySelectorAll('[data-location]');
+        let all = {};
 
         function removeDuplicates(events) {
             return events.filter((elem, pos, arr) => {
@@ -20,15 +20,15 @@ function initMap() {
             return events;
         }
 
-        for(let i = 0; i < locations.length; ++i) {
-			let latLong = locations[i].getAttribute('data-location').split(',');
-			let place = locations[i].getAttribute('data-place');
-			let events = removeDuplicates(aggregateEvents(place, year)).reverse();
+        for (let i = 0; i < locations.length; ++i) {
+            let latLong = locations[i].getAttribute('data-location').split(',');
+            let place = locations[i].getAttribute('data-place');
+            let events = removeDuplicates(aggregateEvents(place, year)).reverse();
 
             all[`[${events}] @ ${place}`] = {lat: parseFloat(latLong[0]), lng: parseFloat(latLong[1])};
         }
-		return all
-	}
+        return all
+    }
 
     let map = new google.maps.Map(document.getElementById('map'), {
         zoom: 3,
@@ -37,13 +37,13 @@ function initMap() {
 
     const urlParams = new URLSearchParams(window.location.search);
     let places = findPlaces(urlParams.get('year'));
-	for(let place in places) {
-		if(places.hasOwnProperty(place)) {
-			new google.maps.Marker({
-				  position: places[place],
-				  map: map,
-				  title: place
-			});
-		}
-	}
+    for (let place in places) {
+        if (places.hasOwnProperty(place)) {
+            new google.maps.marker.AdvancedMarkerElement({
+                position: places[place],
+                map: map,
+                title: place
+            });
+        }
+    }
 }
