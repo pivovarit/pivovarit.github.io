bd399705a4ab3a7ddc098b9092c0fc4952a902f8
Grzegorz Piwowarek <gpiwowarek@gmail.com>
2024-04-19 08:55:54 +0200

    refactor

--- talks/index.js
+++ talks/index.js
@@ -9,28 +9,32 @@ async function initMap() {
             });
         }
 
-        function aggregateEvents(place, year) {
-            let events = [];
+        function aggregateEvents(year) {
+            let results = new Map()
             for (let i = 0; i < locations.length; ++i) {
                 let eventYear = locations[i].nextElementSibling.nextElementSibling.textContent.split('.')[2];
-                if (place === locations[i].getAttribute('data-place') && (!year || year === eventYear)) {
-                    events.push(locations[i].innerHTML)
+                if (!year || year === eventYear) {
+                    let place = {
+                        place: locations[i].getAttribute('data-place'),
+                        latLong: locations[i].getAttribute('data-location').split(',')
+                    };
+                    if (!results.has(place)) {
+                        results.set(place, [])
+                    }
+                    let events = results.get(place);
+                    if (!events.includes(locations[i].innerHTML)) {
+                        events.push(locations[i].innerHTML)
+                    }
                 }
             }
-            return events;
+            return results
         }
 
-        for (let i = 0; i < locations.length; ++i) {
-            let eventYear = locations[i].nextElementSibling.nextElementSibling.textContent.split('.')[2];
-            if (year && year !== eventYear) {
-                continue;
-            }
-            let latLong = locations[i].getAttribute('data-location').split(',');
-            let place = locations[i].getAttribute('data-place');
-            let events = removeDuplicates(aggregateEvents(place, year)).reverse();
+        aggregateEvents(year)
+            .forEach((events, place) => {
+                all[`[${events}] @ ${place.place}`] = {lat: parseFloat(place.latLong[0]), lng: parseFloat(place.latLong[1])};
+            })
 
-            all[`[${events}] @ ${place}`] = {lat: parseFloat(latLong[0]), lng: parseFloat(latLong[1])};
-        }
         return all
     }
 
@@ -43,17 +47,18 @@ async function initMap() {
             mapId: "talksMapId"
         });
         const urlParams = new URLSearchParams(window.location.search);
-        findPlaces(urlParams.get('year')).then(places => {
-            for (let place in places) {
-                if (places.hasOwnProperty(place)) {
-                    new google.maps.marker.AdvancedMarkerElement({
-                        position: places[place],
-                        map: map,
-                        title: place
-                    });
+        findPlaces(urlParams.get('year'))
+            .then(places => {
+                for (let place in places) {
+                    if (places.hasOwnProperty(place)) {
+                        new google.maps.marker.AdvancedMarkerElement({
+                            position: places[place],
+                            map: map,
+                            title: place
+                        });
+                    }
                 }
-            }
-        })
+            })
     }
 
     load()
